9db2ea0608d5ffa654a84cfb890b81edc91025a4
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2025-04-15 22:10:28 +0200

    added particle system class

---
+++ Rendering-Project/Rendering-Project/ParticleSystem.cpp
@@ -0,0 +1,70 @@
+#include "ParticleSystem.hpp"
+#include "ReadFile.hpp"
+
+ParticleSystem::ParticleSystem() {
+    this->shaderPaths = {
+        {"VertexShader", L"ParticleVS.cso"},
+        {"PixelShader", L"ParticlePS.cso"},
+        {"GeometryShader", L"ParticleGS.cso"},
+        {"ComputeShader", L"ParticleCS.cso"},
+    };
+}
+
+HRESULT ParticleSystem::Initialize(Microsoft::WRL::ComPtr<ID3D11Device> device, UINT size, UINT nrOf, bool dynamic,
+                                   bool hasSRV, bool hasUAV) {
+    this->device = device;
+    device->GetImmediateContext(this->immediateContext.GetAddressOf());
+
+    HRESULT result = this->particleBuffer.Create(device, size, nrOf, dynamic, hasSRV, hasUAV);
+    if (FAILED(result)) {
+        return result;
+    }
+
+    return S_OK;
+}
+
+HRESULT ParticleSystem::LoadShaders() {
+    for (const auto& [shaderType, filePath] : this->shaderPaths) {
+        std::string byteData;
+        if (!CM::ReadFile(std::string(filePath.begin(), filePath.end()), byteData)) {
+            std::cerr << "Failed to read shader file: " << std::string(filePath.begin(), filePath.end()) << std::endl;
+            return E_FAIL;
+        }
+
+        HRESULT result = E_FAIL;
+        if (shaderType == "VertexShader") {
+            result = this->device->CreateVertexShader(byteData.data(), byteData.size(), nullptr,
+                                                      this->vertexShader.GetAddressOf());
+        } else if (shaderType == "PixelShader") {
+            result = this->device->CreatePixelShader(byteData.data(), byteData.size(), nullptr,
+                                                     this->pixelShader.GetAddressOf());
+        } else if (shaderType == "GeometryShader") {
+            result = this->device->CreateGeometryShader(byteData.data(), byteData.size(), nullptr,
+                                                        this->geometryShader.GetAddressOf());
+        } else if (shaderType == "ComputeShader") {
+            result = this->device->CreateComputeShader(byteData.data(), byteData.size(), nullptr,
+                                                       this->computeShader.GetAddressOf());
+        }
+
+        if (FAILED(result)) {
+            std::cerr << "Failed to create " << shaderType << "!\n";
+            return result;
+        }
+    }
+
+    if (this->vertexShader) {
+        this->immediateContext->VSSetShader(this->vertexShader.Get(), nullptr, 0);
+    }
+    if (this->pixelShader) {
+        this->immediateContext->PSSetShader(this->pixelShader.Get(), nullptr, 0);
+    }
+    if (this->geometryShader) {
+        this->immediateContext->GSSetShader(this->geometryShader.Get(), nullptr, 0);
+    }
+
+    return S_OK;
+}
+
+ID3D11ShaderResourceView* ParticleSystem::GetSRV() const { return this->particleBuffer.GetSRV(); }
+
+ID3D11UnorderedAccessView* ParticleSystem::GetUAV() const { return this->particleBuffer.GetUAV(); }
---
+++ Rendering-Project/Rendering-Project/ParticleSystem.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "ParticleBuffer.hpp"
+#include <d3d11.h>
+#include <string>
+#include <unordered_map>
+#include <wrl/client.h>
+
+class ParticleSystem {
+  public:
+    ParticleSystem();
+    ~ParticleSystem() = default;
+
+    HRESULT Initialize(Microsoft::WRL::ComPtr<ID3D11Device> device, UINT size, UINT nrOf, bool dynamic, bool hasSRV,
+                       bool hasUAV);
+    HRESULT LoadShaders();
+    ID3D11ShaderResourceView* GetSRV() const;
+    ID3D11UnorderedAccessView* GetUAV() const;
+
+  private:
+    ParticleBuffer particleBuffer;
+    std::unordered_map<std::string, std::wstring> shaderPaths;
+
+    Microsoft::WRL::ComPtr<ID3D11Device> device;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> immediateContext;
+
+    Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
+    Microsoft::WRL::ComPtr<ID3D11GeometryShader> geometryShader;
+    Microsoft::WRL::ComPtr<ID3D11ComputeShader> computeShader;
+};
